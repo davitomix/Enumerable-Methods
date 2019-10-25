9e1f297fdb5604ddcfbfecaa3f1c903bc6ead956
davitomix <davidelimg1mail.com>
2019-10-25 15:33:30 -0500

    fixing some stickler issues

--- Enumerable.rb
+++ Enumerable.rb
@@ -189,7 +189,7 @@ p [1, 2, 3, 4].my_map #=> Enumerator
 puts '---------------------------------------------'
 puts 'my_inject'
 puts (5..10).my_inject { |sum, n| sum + n } #=> 45
-puts (5..10).my_inject :+     #=> 45
+puts (5..10).my_inject :+ #=> 45
 puts (5..10).inject(1) { |product, n| product * n } #=> 151200
 puts (5..10).my_inject(1, :*) #=> 151200
 longest = %w[cat sheep bear].inject do |memo, word|
