dbd065c517d3181a764b94f994002ffb8b76f150
David Elì <davidelimg1@gmail.com>
2019-11-05 17:11:47 -0600

    my_each test added

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -3,8 +3,18 @@
 require_relative '../enumerable.rb'
 
 describe Enumerable do
-  
+  let(:sample_arr) { [1, 2, 3, 4, 5] }
+
   describe '#my_each' do
+    context 'If block is given' do
+      it 'Travels the array, one element at a time' do
+        arr_one = []
+        arr_two = []
+        sample_arr.each { |i| arr_one << i * i }
+        sample_arr.each { |i| arr_two << i * i }
+        expect(arr_one).to eql(arr_two)
+      end
+    end
   end
 
 end
