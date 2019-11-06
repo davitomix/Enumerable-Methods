4cd27cb2b42a5c5da599d780203e8aed3eae2df4
David Elì <davidelimg1@gmail.com>
2019-11-05 19:51:29 -0600

    my_each method tests added

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -11,10 +11,17 @@ describe Enumerable do
         arr_one = []
         arr_two = []
         sample_arr.each { |i| arr_one << i * i }
-        sample_arr.each { |i| arr_two << i * i }
+        sample_arr.my_each { |i| arr_two << i * i }
         expect(arr_one).to eql(arr_two)
       end
     end
+
+    context 'If no block is given' do
+      it 'returns an enumerator' do
+        expect(sample_arr.my_each.is_a?(Enumerable)).to be true
+      end
+    end
   end
+
 
 end
