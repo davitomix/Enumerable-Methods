9977f33fdf49deb55795b5774492d317df5c3859
ampaire <femayapr@gmail.com>
2019-11-08 15:15:36 -0600

    Add tests for the my_select method

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -10,10 +10,11 @@ describe Enumerable do
   let(:nil_arr) { [] }
   hash_one = {}
   hash_two = {}
+  
   describe '#my_each' do
     context 'If block is given' do
       subject { sample_arr.each { |i| i * i } }
-      it 'Travels the arra, one element at a time' do
+      it 'Travels the array, one element at a time' do
         expect(subject).to eql(sample_arr.my_each { |i| i * i})
       end
     end
@@ -39,6 +40,26 @@ describe Enumerable do
       subject { sample_arr.my_each_with_index }
       it 'Returns an enumerator' do
         expect(subject).to be_a(Enumerable)
+      end
+    end
+  end
+
+
+  describe '#my_select'  do
+    it 'Returns the selected item according to the specified conditions' do
+      original_arr = sample_arr.my_select{ |x| x > 2 }
+      test_arr = sample_arr.my_select{ |x| x > 2 }
+      expect(test_arr).to eql(original_arr)
+    end
+
+    context "If a block is given" do
+      it 'Returns the selected item' do
+      end
+    end
+    
+    context "If no block is given" do
+      it 'Returns an enumerator' do
+        expect(sample_arr.my_each.is_a?(Enumerable)).to be true
       end
     end
   end
