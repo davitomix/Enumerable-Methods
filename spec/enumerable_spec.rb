08d1da9dfdd353281724ad948b6914b1ac705ec5
David Elì <davidelimg1@gmail.com>
2019-11-08 14:01:06 -0600

    Working on test for my_each_with_index method

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -8,7 +8,8 @@ describe Enumerable do
   let(:mix_arr) { [nil, true, 99] }
   let(:num_arr) { [1, 3.14, 2i] }
   let(:nil_arr) { [] }
-
+  hash_one = {}
+  hash_two = {}
   describe '#my_each' do
     context 'If block is given' do
       subject { sample_arr.each { |i| i * i } }
@@ -22,6 +23,16 @@ describe Enumerable do
     subject { sample_arr.my_each.is_a?(Enumerable) }
     it 'returns an enumerator' do
       expect(subject).to be true
+    end
+  end
+
+  describe '#my_each_with_index' do
+    context 'If block is given' do
+      it 'Travels the array, one element at a time with index' do
+        sample_arr.each_with_index{ |i, indx| hash_one[i] = indx } 
+        sample_arr.my_each_with_index{ |i, indx| hash_two[i] = indx } 
+        expect(hash_one).to eql(hash_two)
+      end
     end
   end
 end
